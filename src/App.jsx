7bd748efffd51a96d0286c5e66e273fd01b94b43
Mohammad Uvais <mohammad.uvais@healthmug.com>
2024-12-09 18:48:23 +0530

    commit from office

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,33 @@ import "./App.css";
 function App() {
   return (
     <>
-      <header>
-        <span>{"{ MU }"}</span> <span className="download-icon">Resume</span>
+      <header class="header">
+        <span className="logo">{"{ }"}</span>
+        <span className="download-resume">
+          <button>Resume</button>
+        </span>
       </header>
       <main>
-        <h1>Don't Look into my screen</h1>
-        <p>React Developer</p>
-        <section>
-          <a href="https://www.linkedin.com/in/mohammaduvais">LinkedIn</a>
-          <a href="https://github.com/mohammad-uvais">GitHub</a>
-          {/* contact svgs */}
+        <section className="hero-content">
+          <article className="introduction">
+            <h1>Mohammad Uvais</h1>
+            <p className="profession">React Developer</p>
+            <article className="contact-links">
+              <a href="https://www.linkedin.com/in/mohammaduvais">LinkedIn</a>
+              <a href="https://github.com/mohammad-uvais">GitHub</a>
+            </article>
+            <article className="action-btn">
+              <button>Let's work together</button>
+              <button>Know more about Uvais</button>
+            </article>
+          </article>
+          <article className="technologies">
+            <p>Technologies</p>
+          </article>
         </section>
-        <section>
-          <button>Let's work together</button>
-          <button>Know more about Uvais</button>
-        </section>
+        <div style={{ border: "1px solid yellow" }}>Other content</div>
       </main>
-      <footer>additional info</footer>
+      {/* <footer>additional info</footer> */}
     </>
   );
 }
