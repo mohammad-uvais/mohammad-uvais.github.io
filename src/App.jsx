bded87cd298e686e78c2ba73af773e7d7004c2d9
Mohammad Uvais <mohammad.uvais@healthmug.com>
2024-12-12 18:58:30 +0530

    changes in technologies

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,10 @@ function App() {
       </header>
       <main>
         <section className="hero-content">
-          <article className="introduction">
-            <h1>Mohammad Uvais</h1>
+          <div className="introduction">
+            <h1>Wolverine</h1>
             <p className="profession">Front End Developer</p>
-            <article className="contact-links">
+            <div className="contact-links">
               <a
                 href="https://www.linkedin.com/in/mohammaduvais"
                 target="_blank"
@@ -32,17 +32,27 @@ function App() {
               <a href="https://github.com/mohammad-uvais" target="_blank">
                 GitHub
               </a>
-            </article>
-            <article className="action-btn">
+            </div>
+            <div className="action-btn">
               <button onClick={handleWorkTogether}>Let's work together</button>
-              <button onClick={handleKnowMore}>Know more about Uvais</button>
-            </article>
-          </article>
-          <article className="technologies">
-            {/* <h1>Mohammad Uvais</h1> */}
-          </article>
+              <button onClick={handleKnowMore} className="know-more">
+                Know more about Uvais
+              </button>
+            </div>
+          </div>
+          <div className="technologies">
+            <h2 style={{ textAlign: "center" }}>Technologies</h2>
+            <div className="technology-items">
+              <p>React</p>
+              <p>React Native</p>
+              <p>Redux</p>
+              <p>JavaScript</p>
+              <p>CSS</p>
+              <p>HTML</p>
+            </div>
+          </div>
         </section>
-        <div style={{ border: "1px solid yellow" }}>Other content</div>
+        <section className="projects-section">Projects</section>
       </main>
       <footer>additional info</footer>
     </>
@@ -50,15 +60,3 @@ function App() {
 }
 
 export default App;
-{
-  /* <h1>Mohammad Uvais</h1>
-            <p className="profession">React Developer</p>
-            <article className="contact-links">
-              <a href="https://www.linkedin.com/in/mohammaduvais">LinkedIn</a>
-              <a href="https://github.com/mohammad-uvais">GitHub</a>
-            </article>
-            <article className="action-btn">
-              <button>Let's work together</button>
-              <button>Know more about Uvais</button>
-            </article> */
-}
